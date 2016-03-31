60cd1eb415af436761a4ba92d854c8aa47bd6c8b
john carlo franco <johncarlo_franco@yahoo.com>
2016-03-30 22:47:29 -0700

    camera view added

--- app/src/main/java/com/apkmarvel/cardboardsample/MainActivity.java
+++ app/src/main/java/com/apkmarvel/cardboardsample/MainActivity.java
@@ -14,64 +14,246 @@ import com.google.vrtoolkit.cardboard.Viewport;
 
 import javax.microedition.khronos.egl.EGLConfig;
 
-public class MainActivity extends CardboardActivity implements CardboardView.StereoRenderer {
-private static final String TAG = MainActivity.class.getSimpleName();
-private CardboardOverlayView overlayView;
-@Override
-public void onCreate(Bundle savedInstanceState) {
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+import java.nio.ShortBuffer;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import android.graphics.SurfaceTexture;
+import android.graphics.SurfaceTexture.OnFrameAvailableListener;
+import android.hardware.Camera;
+import android.opengl.GLES20;
+import android.opengl.Matrix;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.vrtoolkit.cardboard.CardboardActivity;
+import com.google.vrtoolkit.cardboard.CardboardView;
+import com.google.vrtoolkit.cardboard.HeadTransform;
+import com.google.vrtoolkit.cardboard.Viewport;
+
+public class MainActivity extends CardboardActivity implements CardboardView.StereoRenderer,OnFrameAvailableListener {
+
+    private static final String TAG = "MainActivity";
+    private static final int GL_TEXTURE_EXTERNAL_OES = 0x8D65;
+    private Camera camera;
+
+    private final String vertexShaderCode =
+            "attribute vec4 position;" +
+                    "attribute vec2 inputTextureCoordinate;" +
+                    "varying vec2 textureCoordinate;" +
+                    "void main()" +
+                    "{"+
+                    "gl_Position = position;"+
+                    "textureCoordinate = inputTextureCoordinate;" +
+                    "}";
+
+    private final String fragmentShaderCode =
+            "#extension GL_OES_EGL_image_external : require\n"+
+                    "precision mediump float;" +
+                    "varying vec2 textureCoordinate;                            \n" +
+                    "uniform samplerExternalOES s_texture;               \n" +
+                    "void main(void) {" +
+                    "  gl_FragColor = texture2D( s_texture, textureCoordinate );\n" +
+                    //"  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n" +
+                    "}";
+
+    private FloatBuffer vertexBuffer, textureVerticesBuffer;
+    private ShortBuffer drawListBuffer;
+    private int mProgram;
+    private int mPositionHandle;
+    private int mTextureCoordHandle;
+
+    // number of coordinates per vertex in this array
+    static final int COORDS_PER_VERTEX = 2;
+    static float squareVertices[] = { // in counterclockwise order:
+            -1.0f, -1.0f,   // 0.left - mid
+            1.0f, -1.0f,   // 1. right - mid
+            -1.0f, 1.0f,   // 2. left - top
+            1.0f, 1.0f,   // 3. right - top
+    };
+
+    private short drawOrder[] =  {0, 2, 1, 1, 2, 3 }; // order to draw vertices
+    static float textureVertices[] = {
+            0.0f, 1.0f,  // A. left-bottom
+            1.0f, 1.0f,  // B. right-bottom
+            0.0f, 0.0f,  // C. left-top
+            1.0f, 0.0f   // D. right-top
+    };
+
+    private final int vertexStride = COORDS_PER_VERTEX * 4; // 4 bytes per vertex
+    private int texture;
+    private CardboardView cardboardView;
+    private SurfaceTexture surface;
+    private float[] mView;
+    private float[] mCamera;
+
+    public void startCamera(int texture)
+    {
+        surface = new SurfaceTexture(texture);
+        surface.setOnFrameAvailableListener(this);
+
+        camera = Camera.open();
+
+        try
+        {
+            camera.setPreviewTexture(surface);
+            camera.startPreview();
+        }
+        catch (IOException ioe)
+        {
+            Log.w("MainActivity","CAM LAUNCH FAILED");
+        }
+    }
+
+    static private int createTexture()
+    {
+        int[] texture = new int[1];
+
+        GLES20.glGenTextures(1, texture, 0);
+        GLES20.glBindTexture(GL_TEXTURE_EXTERNAL_OES, texture[0]);
+        GLES20.glTexParameterf(GL_TEXTURE_EXTERNAL_OES,
+                GL10.GL_TEXTURE_MIN_FILTER,GL10.GL_LINEAR);
+        GLES20.glTexParameterf(GL_TEXTURE_EXTERNAL_OES,
+                GL10.GL_TEXTURE_MAG_FILTER, GL10.GL_LINEAR);
+        GLES20.glTexParameteri(GL_TEXTURE_EXTERNAL_OES,
+                GL10.GL_TEXTURE_WRAP_S, GL10.GL_CLAMP_TO_EDGE);
+        GLES20.glTexParameteri(GL_TEXTURE_EXTERNAL_OES,
+                GL10.GL_TEXTURE_WRAP_T, GL10.GL_CLAMP_TO_EDGE);
+
+        return texture[0];
+    }
+
+
+    /**
+     * Converts a raw text file, saved as a resource, into an OpenGL ES shader
+     * @param type The type of shader we will be creating.
+     * @return
+     */
+    private int loadGLShader(int type, String code) {
+        int shader = GLES20.glCreateShader(type);
+        GLES20.glShaderSource(shader, code);
+        GLES20.glCompileShader(shader);
+
+        // Get the compilation status.
+        final int[] compileStatus = new int[1];
+        GLES20.glGetShaderiv(shader, GLES20.GL_COMPILE_STATUS, compileStatus, 0);
+
+        // If the compilation failed, delete the shader.
+        if (compileStatus[0] == 0) {
+            Log.e(TAG, "Error compiling shader: " + GLES20.glGetShaderInfoLog(shader));
+            GLES20.glDeleteShader(shader);
+            shader = 0;
+        }
+
+        if (shader == 0) {
+            throw new RuntimeException("Error creating shader.");
+        }
+
+        return shader;
+    }
+
+    /**
+     * Sets the view to our CardboardView and initializes the transformation matrices we will use
+     * to render our scene.
+     * @param savedInstanceState
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
-        CardboardView cardboardView = (CardboardView) findViewById(R.id.cardboard_view);
-        cardboardView.setRestoreGLStateEnabled(false);
+        cardboardView = (CardboardView) findViewById(R.id.cardboard_view);
         cardboardView.setRenderer(this);
         setCardboardView(cardboardView);
 
-        overlayView = (CardboardOverlayView) findViewById(R.id.overlay);
-        overlayView.show3DToast("Pull the magnet when you find an object.");
-        }
+        mCamera = new float[16];
+        mView = new float[16];
+    }
 
-@Override
-public void onPause() {
-        super.onPause();
-        }
+    @Override
+    public void onRendererShutdown() {
+        Log.i(TAG, "onRendererShutdown");
+    }
+
+    @Override
+    public void onSurfaceChanged(int width, int height) {
+        Log.i(TAG, "onSurfaceChanged");
+    }
+
+    /**
+     * Creates the buffers we use to store information about the 3D world. OpenGL doesn't use Java
+     * arrays, but rather needs data in a format it can understand. Hence we use ByteBuffers.
+     * @param config The EGL configuration used when creating the surface.
+     */
+    @Override
+    public void onSurfaceCreated(EGLConfig config) {
+        Log.i(TAG, "onSurfaceCreated");
+//        GLES20.glClearColor(0.1f, 0.1f, 0.1f, 0.5f); // Dark background so text shows up well
+
+        ByteBuffer bb = ByteBuffer.allocateDirect(squareVertices.length * 4);
+        bb.order(ByteOrder.nativeOrder());
+        vertexBuffer = bb.asFloatBuffer();
+        vertexBuffer.put(squareVertices);
+        vertexBuffer.position(0);
 
 
-@Override
-public void onSurfaceChanged(int width, int height) {
-        Log.e(TAG, "onSurfaceChanged");
-        }
-
-@Override
-public void onSurfaceCreated(EGLConfig eglConfig) {
-
-        }
-
-@Override
-public void onRendererShutdown() {
-        Log.e(TAG,"onRendererShutdown");
-        }
+        ByteBuffer dlb = ByteBuffer.allocateDirect(drawOrder.length * 2);
+        dlb.order(ByteOrder.nativeOrder());
+        drawListBuffer = dlb.asShortBuffer();
+        drawListBuffer.put(drawOrder);
+        drawListBuffer.position(0);
 
 
-@Override
-public void onNewFrame(HeadTransform headTransform) {
+        ByteBuffer bb2 = ByteBuffer.allocateDirect(textureVertices.length * 4);
+        bb2.order(ByteOrder.nativeOrder());
+        textureVerticesBuffer = bb2.asFloatBuffer();
+        textureVerticesBuffer.put(textureVertices);
+        textureVerticesBuffer.position(0);
 
-        }
+        int vertexShader = loadGLShader(GLES20.GL_VERTEX_SHADER, vertexShaderCode);
+        int fragmentShader = loadGLShader(GLES20.GL_FRAGMENT_SHADER, fragmentShaderCode);
 
-@Override
-public void onDrawEye(Eye eye) {
+        mProgram = GLES20.glCreateProgram();             // create empty OpenGL ES Program
+        GLES20.glAttachShader(mProgram, vertexShader);   // add the vertex shader to program
+        GLES20.glAttachShader(mProgram, fragmentShader); // add the fragment shader to program
+        GLES20.glLinkProgram(mProgram);
 
-        }
-@Override
-public void onFinishFrame(Viewport viewport) {
-        Log.e(TAG,"onFinishFrame");
-        }
+        texture = createTexture();
+        startCamera(texture);
+    }
+    @Override
+    public void onNewFrame(HeadTransform headTransform) {
+        float[] mtx = new float[16];
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
+        surface.updateTexImage();
+        surface.getTransformMatrix(mtx);
 
-/**
- * Called when the Cardboard trigger is pulled.
- */
-@Override
-public void onCardboardTrigger() {
-        Log.e(TAG,"onCardboardTrigger");
-        }
+    }
 
-        }
+    @Override
+    public void onFrameAvailable(SurfaceTexture arg0) {
+        this.cardboardView.requestRender();
+
+    }
+    @Override
+    public void onDrawEye(Eye transform) {
+        GLES20.glUseProgram(mProgram);
+        GLES20.glEnableVertexAttribArray(mPositionHandle);
+        GLES20.glVertexAttribPointer(mPositionHandle, COORDS_PER_VERTEX, GLES20.GL_FLOAT,false,vertexStride, vertexBuffer);
+        mTextureCoordHandle = GLES20.glGetAttribLocation(mProgram, "inputTextureCoordinate");
+        GLES20.glEnableVertexAttribArray(mTextureCoordHandle);
+        GLES20.glVertexAttribPointer(mTextureCoordHandle, COORDS_PER_VERTEX, GLES20.GL_FLOAT,false,vertexStride, textureVerticesBuffer);
+        GLES20.glDrawElements(GLES20.GL_TRIANGLES, drawOrder.length,GLES20.GL_UNSIGNED_SHORT, drawListBuffer);
+    }
+    @Override
+    public void onFinishFrame(Viewport viewport) {
+    }
+    @Override
+    public void onCardboardTrigger() {
+    }
+}
